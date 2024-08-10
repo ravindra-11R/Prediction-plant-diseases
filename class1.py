7b25bd3d126a44c46b3ec18bf6447128389da324
ravindra-11R <55081616+ravindra-11R@users.noreply.github.com>
2024-08-10 21:16:41 +0100

    Update class1.py

--- class1.py
+++ class1.py
@@ -12,8 +12,8 @@ output = 'best_custom_model.h5'
 # Function to download the model
 def download_model():
     try:
-        gdown.download(f'https://drive.google.com/uc?export=download&id=1sZA79FXoZ12YnUoB7VGXI49-wMWPtCY6
-', output, quiet=False)
+        gdown.download(f"https://drive.google.com/uc?export=download&id=1sZA79FXoZ12YnUoB7VGXI49-wMWPtCY6
+", output, quiet=False)
         if not os.path.exists(output):
             st.error("Model file not found after download.")
         else:
